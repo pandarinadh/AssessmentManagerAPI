9ff819ae4b2504ad7ba02b3e9e3a461daff2211e
pandarinadh <39387722+pandarinadh@users.noreply.github.com>
2018-05-18 10:00:20 -0400

    Initial commit

---
+++ AssessmentManager/Controllers/AssessmentCenterController.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("api/AssesssmentCenter")]
+    public class AssessmentCenterController : ApiController
+    {
+        public string strFile = @"C:\my\Practice\ns-question\apidata\studentAssessmentData.js";
+
+       
+
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        [Route("GetAllStudentAssessments")]
+        public List<StudentAssessment> GetAllStudentAssessments()
+        {
+            List<StudentAssessment> _data = getData();
+
+            return _data;
+        }
+
+        private List<StudentAssessment> getData()
+        {
+
+            List<StudentAssessment> _data = new List<StudentAssessment>();
+            using (StreamReader r = new StreamReader(strFile))
+            {
+                string strJson = r.ReadToEnd();
+                if (strJson != "")
+                {
+                    _data = JsonConvert.DeserializeObject<List<StudentAssessment>>(strJson);
+                }
+            }
+
+            return _data;
+        }
+
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpPost]
+        [Route("save")]
+        public StudentAssessment save(StudentAssessment studentAssessment)
+        {
+            List<StudentAssessment> _data = getData();
+
+            if (_data == null) _data = new List<StudentAssessment>();
+
+
+            //  question.Id = "999";
+
+
+            if (studentAssessment != null)
+            {
+                if (studentAssessment.Id != 0)
+                {
+                    var tempQ = _data.FirstOrDefault(r => r.Id == studentAssessment.Id);
+                    tempQ.Student.Assessments = studentAssessment.Student.Assessments;
+                }
+                else
+                {
+                    if (_data.Count == 0)
+                        studentAssessment.Id = 1;
+                    else
+                        studentAssessment.Id = (Convert.ToInt32(_data.Max(r => r.Id)) + 1);
+
+                    _data.Add(studentAssessment);
+                }
+
+            }
+            else
+                throw new Exception("error");
+
+            string json = JsonConvert.SerializeObject(_data.ToArray(), Formatting.Indented);
+
+            //write string to file
+            System.IO.File.WriteAllText(strFile, json);
+
+            return studentAssessment;
+        }
+    }
+
+    public class StudentAssessment
+    {
+        public int Id { get; set; }
+        public Student Student { get; set; }
+    }
+}
---
+++ AssessmentManager/Controllers/AssessmentController.cs
@@ -0,0 +1,136 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("api/assessment")]
+    public class AssessmentController : ApiController
+    {
+        public string strFile = @"C:\my\Practice\ns-question\apidata\assessmentData.js";
+
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpPost]
+        [Route("saveAssessment")]
+        public Assessment saveAssessment(Assessment assessment)
+        {
+            List<Assessment> _data = getData();
+
+            if (_data == null) _data = new List<Assessment>();
+
+
+            //  assessment.Id = "999";
+
+
+            if (assessment != null)
+            {
+                if (assessment.Id != 0)
+                {
+                    var tempQ = _data.FirstOrDefault(r => r.Id == assessment.Id);
+                    tempQ.Text = assessment.Text;
+                    tempQ.Description = assessment.Description;
+                    tempQ.Questions = assessment.Questions;
+                }
+                else
+                {
+                    if (_data.Count == 0)
+                        assessment.Id = 1;
+                    else
+                        assessment.Id = (Convert.ToInt32(_data.Max(r => r.Id)) + 1);
+
+                    _data.Add(assessment);
+                }
+
+            }
+            else
+                throw new Exception("error");
+
+            string json = JsonConvert.SerializeObject(_data.ToArray(), Formatting.Indented);
+
+            //write string to file
+            System.IO.File.WriteAllText(strFile, json);
+
+            return assessment;
+        }
+
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        [Route("GetAllAssessments")]
+        public List<Assessment> GetAllAssessments()
+        {
+            List<Assessment> _data = getData();
+
+            return _data;
+        }
+
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        [Route("GetAssessmentById")]
+        public Assessment GetAssessmentById(int assessmentId)
+        {
+            List<Assessment> _data = getData();
+
+            var assessment = _data.FirstOrDefault(r => r.Id == assessmentId);
+
+            return assessment;
+        }
+
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        [Route("DeleteAssessment")]
+        public Assessment DeleteAssessment(int assessmentId)
+        {
+            List<Assessment> _data = getData();
+
+            var assessment = _data.FirstOrDefault(r => r.Id == assessmentId);
+            var removedAssessment = assessment;
+            _data.Remove(assessment);
+
+            string json = JsonConvert.SerializeObject(_data.ToArray(), Formatting.Indented);
+
+            //write string to file
+            System.IO.File.WriteAllText(strFile, json);
+
+            return removedAssessment;
+        }
+
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        public string Get()
+        {
+
+            return "success";
+        }
+
+        private List<Assessment> getData()
+        {
+
+            List<Assessment> _data = new List<Assessment>();
+            using (StreamReader r = new StreamReader(strFile))
+            {
+                string strJson = r.ReadToEnd();
+                if (strJson != "")
+                {
+                    _data = JsonConvert.DeserializeObject<List<Assessment>>(strJson);
+                }
+            }
+
+            return _data;
+        }
+    }
+
+    public class Assessment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+        public IList<Question> Questions { get; set; }
+    }
+}
